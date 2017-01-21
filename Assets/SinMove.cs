7070706286c042e7f9c304f6fbfc56ca0c8db36d
Alix <alix.fumoleau@gmail.com>
2017-01-21 17:52:21 +0100

    Home menu design

---
+++ Assets/SinMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SinMove : MonoBehaviour {
+
+	Vector3 initialPosition;
+
+	public float timeScale;
+	public float xOffset;
+	public float xAmplitude;
+	public float yOffset;
+	public float yAmplitude;
+
+	void Start() {
+		initialPosition = transform.localPosition;
+	}
+
+	void Update () {
+		transform.localPosition = initialPosition + new Vector3(xAmplitude * Mathf.Sin(xOffset + timeScale * Time.time),
+																yAmplitude * Mathf.Sin(yOffset + timeScale * Time.time),
+																0);
+	}
+}
--- Assets/WavesManager.cs
+++ Assets/WavesManager.cs
@@ -5,10 +5,6 @@ public class WavesManager : MonoBehaviour {
 	[Range(0f, 0.5f)] public float waveAmplitude;
 	[SerializeField] MeshRenderer seaRendering;
 	[SerializeField] SurfaceLevel seaPhysics;
-
-	void Start () {
-		
-	}
 	
 	void Update () {
 		seaPhysics.amplitude = waveAmplitude;
