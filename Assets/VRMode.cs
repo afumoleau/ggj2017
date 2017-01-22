9d1ad9a80e63604d36202d4e448660b694e18032
Alix <alix.fumoleau@gmail.com>
2017-01-22 16:40:13 +0100

    Enable VR Mode, add screenshots

---
+++ Assets/VRMode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VRMode : MonoBehaviour {
+
+	public Camera classicCamera;
+	public Camera vrCamera;
+	public RawImage leftEye;
+	public RawImage rightEye;
+	public GameObject vrCanvas;
+
+	void OnEnable() {
+		var rt = new RenderTexture(1024, 1024, 32, RenderTextureFormat.ARGB32);
+
+		classicCamera.targetTexture = rt;
+		
+		leftEye.texture = rt;
+		rightEye.texture = rt;
+		vrCanvas.SetActive(true);
+	}
+	void OnDisable() {
+		//var rt = new RenderTexture(1024, 1024, 32, RenderTextureFormat.ARGB32);
+		classicCamera.targetTexture = null;
+		if(vrCanvas != null) {
+			vrCanvas.SetActive(false);
+		}
+	}
+}
